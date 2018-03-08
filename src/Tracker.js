26418ac4b388db67a34c7d5a7a380baa49480725
六娃 <wencheng.lwc@alibaba-inc.com>
2018-03-09 00:01:04 +0800

    fix: rull -> rule

--- src/Tracker.js
+++ src/Tracker.js
@@ -2,19 +2,19 @@ let stackId = 0
 const emptyRecord = record => record === undefined || record === null
 
 export default class Tracker {
-  constructor(rull, record, parent) {
+  constructor(rule, record, parent) {
     this.id = stackId++
     this.parent = parent || null
     this.rootTracker = parent ? parent.rootTracker : this
-    this.rull = rull
+    this.rule = rule
     this.record = record
     this.depth = parent ? parent.depth + 1 : 0
   }
   add(data) {
-    if (!this.rull) return this
-    const record = this.rull(data, this)
+    if (!this.rule) return this
+    const record = this.rule(data, this)
     if (emptyRecord(record)) return this
-    return new Tracker(this.rull, record, this)
+    return new Tracker(this.rule, record, this)
   }
   length() {
     return (emptyRecord(this.rootTracker.record) ? 0 : 1) + this.depth
